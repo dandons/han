a632dc9422f123c4e6d83341d5188e7cf4a317ec
Dan <ddonselaar@gmail.com>
2014-09-12 18:49:43 +0200

    Amdahl opdracht v1.0

---
+++ DEA/amdahl_opdracht/amdahl/src/main/java/amdahl/Amdahl.java
@@ -0,0 +1,28 @@
+package amdahl;
+
+public abstract class Amdahl {	
+	
+	/**
+	 * @description Dit is de formulier van Amdahl, de formulier berekent de "Speed Up factor"
+	 * @link http://en.wikipedia.org/wiki/Amdahl's_law
+	 * @param aantalProcessoren
+	 * @param sequentialPrecentage 1.0 = 100% | 0.5 = 50% | 0.0 = 0%
+	 * @return speed up factor in double
+	 * @author Oefening Amdahl week 2 DOA by Daniel Donselaar
+	 */
+	public static double calculateSpeedUp(int aantalProcessoren, double sequentialPrecentage){
+		if(aantalProcessoren <= 0){ 						// Controle of er meer dan 1 processor is.
+			aantalProcessoren = 1;
+		}
+		if(sequentialPrecentage < 0){						// Controle of de percentage minder is dan 0%
+			sequentialPrecentage = 0;
+		}
+		if(sequentialPrecentage > 1){						// Controle of de percentage meer is dan 100%
+			sequentialPrecentage = 1;
+		}
+		return aantalProcessoren / (1 + sequentialPrecentage*(aantalProcessoren - 1));
+	}
+	
+	
+	
+}
---
+++ DEA/amdahl_opdracht/amdahl/src/main/java/amdahl/AmdahlApp.java
@@ -0,0 +1,9 @@
+package amdahl;
+
+public class AmdahlApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(SetuAmdahl.calculateSpeedUp(1000, 0.001));
+    }
+}
